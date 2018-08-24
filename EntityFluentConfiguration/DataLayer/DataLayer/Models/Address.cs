264f9ae8d83dbe608792e085f7c0f5a5f624b04e
MAHARAJAN PANDIAN <maharajanmca2010@gmail.com>
2018-08-24 17:31:57 +0900

    Added EntityFluentConfiguration Project

---
+++ EntityFluentConfiguration/DataLayer/DataLayer/Models/Address.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataLayer.Models
+{
+    public class Address
+    {
+        [Key]
+        public long PK { get; set; }
+        public string StreetName { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
